f13f384d94691f9177c69a3bdda3bcbd5a0ab362
root <root@ip-10-201-26-228.uacslab.uspto.gov>
2020-02-05 20:47:49 +0000

    adding the file

---
+++ gremlinMemoryAttack.sh
@@ -0,0 +1,4 @@
+echo "Adding 6 GB of Memory for Attack "
+curl 'https://api.gremlin.com/v1/attacks/new?teamId=ac1abe28-1321-5d8d-a981-17cbb786830f' -H 'Content-Type: application/json;charset=utf-8' -H 'Authorization: Bearer Yy1iNjFiYzIwMS04NzkwLTUyOTgtYTY3MS0yZDJmZjFmN2NlNGE6c2F0aXNoLmNoZW5udUB1c3B0by5nb3Y6YjdlMTI4NzMtZWNmNi00YmEyLWExMjgtNzNlY2Y2Y2JhMjBj' --data '{"target":{"type":"Random","hosts":{"multiSelectTags":{"local-hostname":["ip-10-0-170-22.ec2.internal"]}},"percent":100},"command":{"type":"memory","commandType":"Memory","args":["-l","60","-g","6"]}}'
+echo "listing the active attacks"
+curl -X GET "https://api.gremlin.com/v1/attacks/active?source=Adhoc&pageSize=1&teamId=ac1abe28-1321-5d8d-a981-17cbb786830f" -H "Authorization: Bearer Yy1iNjFiYzIwMS04NzkwLTUyOTgtYTY3MS0yZDJmZjFmN2NlNGE6c2F0aXNoLmNoZW5udUB1c3B0by5nb3Y6YjdlMTI4NzMtZWNmNi00YmEyLWExMjgtNzNlY2Y2Y2JhMjBj" -H "accept: application/json"
