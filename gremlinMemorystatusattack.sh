e0619be7f90d5553781ad0f910691e0f5cf1fee0
root <root@ip-10-201-26-228.uacslab.uspto.gov>
2020-02-05 21:40:30 +0000

    adding new details

---
+++ gremlinMemorystatusattack.sh
@@ -0,0 +1,2 @@
+echo "listing the active attacks"
+curl -X GET "https://api.gremlin.com/v1/attacks/active?source=Adhoc&pageSize=1&teamId=ac1abe28-1321-5d8d-a981-17cbb786830f" -H "Authorization: Bearer Yy1iNjFiYzIwMS04NzkwLTUyOTgtYTY3MS0yZDJmZjFmN2NlNGE6c2F0aXNoLmNoZW5udUB1c3B0by5nb3Y6YjdlMTI4NzMtZWNmNi00YmEyLWExMjgtNzNlY2Y2Y2JhMjBj" -H "accept: application/json"
