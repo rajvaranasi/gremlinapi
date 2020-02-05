926e06f305e3cfa3d3c5065db8eb62b6301171fa
root <root@ip-10-201-26-228.uacslab.uspto.gov>
2020-02-05 21:59:34 +0000

    adding files

---
+++ gremlinhostshutdown.sh
@@ -0,0 +1,2 @@
+echo "shutting down the host"
+curl 'https://api.gremlin.com/v1/attacks/new?teamId=ac1abe28-1321-5d8d-a981-17cbb786830f' -H 'Content-Type: application/json;charset=utf-8' -H 'Authorization: Bearer Yy1iNjFiYzIwMS04NzkwLTUyOTgtYTY3MS0yZDJmZjFmN2NlNGE6c2F0aXNoLmNoZW5udUB1c3B0by5nb3Y6YjdlMTI4NzMtZWNmNi00YmEyLWExMjgtNzNlY2Y2Y2JhMjBj' --data '{"target":{"type":"Random","hosts":{"multiSelectTags":{"local-hostname":["ip-10-0-167-31.ec2.internal"]}},"percent":100},"command":{"type":"shutdown","commandType":"Shutdown","args":["-d","1","-r"]}}'
